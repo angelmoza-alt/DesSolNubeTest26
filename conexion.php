7a33068d72a6eb864baf3f70793a2e57093d2a98
Victus <angel.moza@tecsup.edu.pe>
2026-08-26 11:06:36 -0500

    Actualizacion de credenciales en conexion.php

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@ Walter Mauro Moncada Rubio 26/08/2026 10:18 a. m. • <?php
 
 function conexion(){
 
-$host = "host=dpg-cvmpjbeuk2gs73f8tqh0-a.oregon-postgres.render.com";
+$host = "host=dpg-da7fkch42hec73baoa0g-a.oregon-postgres.render.com";
 $port = "port=5432";
-$dbname = "dbname=test_bh54";
-$user = "user=wmoncada";
-$password = "password=q9ZlwtfUxwIwwO9fKmsEuQJDMtE6HtcN";
+$dbname = "dbname=test_db_en4e";
+$user = "user=test_db_en4e_user";
+$password = "password=JiVyZZKPIzaCI3YBvHptwRLYrl2ZyYtx";
 
 $db = pg_connect("$host $port $dbname $user $password");
 
